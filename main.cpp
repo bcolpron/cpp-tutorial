12cd2884bd2b2847ec141e4e0321c95938daf221
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-11 15:26:53 -0500

    basic dont-do-this memory management example
        pick fd64c06 'bomb is set' comment

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
-int main(int, const char*[])
+struct Blob
 {
-    std::cout << "hello" << std::endl;
+    std::string name = "Inigo Montoya"; 
+    /* More stuff here */
+};
+
+class Widget
+{
+    Blob* blob;
+public:
+    Widget()
+    {
+        blob = new Blob();
+    }
+    ~Widget()
+    {
+        delete blob;
+    }
+
+    void say_my_name() { std::cout << "Hello, my name is " << blob->name << std::endl; }
+};
+
+
+int main(int, const char *[])
+{
+    Widget w;
+    w.say_my_name();
     return 0;
 }
