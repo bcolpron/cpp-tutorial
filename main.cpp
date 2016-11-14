b99a42303de34e4ff799728e451486471db11d58
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-14 12:41:11 -0500

    basic use case

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <cassert>
+#include <map>
+#include <thread>
+
+struct Contact
+{
+    std::string name;
+    std::string phone;
+};
+
+typedef unsigned int ContactId; 
+
+class AddressBook
+{
+public:
+    ContactId add(const Contact& c);
+    Contact get(ContactId id) const;
+    void update(ContactId id, const Contact& c);
+    void delete(ContactId id);
+};
 
 int main(int, const char*[])
 {
-    std::cout << "hello" << std::endl;
+    AddressBook book;
+
     return 0;
 }
