a0a1e3924c19061b4ce442ec4bb792e5fee1e720
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-29 12:26:45 -0500

    simple vector of int

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
 
 int main(int, const char*[])
 {
-    std::cout << "hello" << std::endl;
+    std::vector<int> v = {23, 42, 4, 8};
+    
     return 0;
 }
