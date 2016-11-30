806dce6b57a221499935567d0b79b14feef29bfd
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-30 08:28:16 -0500

    Removing elements off the vector

--- main.cpp
+++ main.cpp
@@ -10,12 +10,7 @@ int main(int, const char*[])
     v.push_back(15);
     v.push_back(16);
 
-    // Print element #4 of v... 
-    std::cout << v[3] << std::endl;
-    std::cout << v.at(3) << std::endl;  // does bound checking!
-    std::cout << *(v.begin() + 3) << std::endl;
-    std::cout << *(begin(v) + 3) << std::endl;
-    std::cout << *(v.data() + 3) << std::endl;  // pointer arithmetic
+    // Removing elements off the vector 
 
     return 0;
 }
