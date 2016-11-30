a7a119c24e4258c37e3fe56e866963bc84c126ea
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-30 08:48:30 -0500

    doesn't compose!!

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ void twice(std::vector<int>& v)
     }
 }
 
+std::vector<int> returns_a_vector();
+
 int main(int, const char*[])
 {
     std::vector<int> v = {23, 42, 4, 8};
@@ -21,6 +23,15 @@ int main(int, const char*[])
 
     // mutating function
     twice(v);
+    twice(v);
+    twice(v);   // cannot chain calls!
+
+    // Cannot pass a temporary
+    twice(returns_a_vector());  // ERROR: initialization of non-const reference
+
+    // Solution: declare a local variable
+    var v2 = returns_a_vector();
+    twice(v2);
 
     return 0;
 }
