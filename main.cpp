8d6f32a9519abb3032e70bebd8c72b8c67cb24f4
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-30 08:44:01 -0500

    mutating function

--- main.cpp
+++ main.cpp
@@ -4,23 +4,13 @@
 #include <algorithm>
 #include <iterator>
 
-void print(const std::vector<int>& v)
-{
-    for(auto item : v)
-    {
-        std::cout << item << " ";
-    }
-    std::cout << std::endl;
-}
-
 int main(int, const char*[])
 {
     std::vector<int> v = {23, 42, 4, 8};
     v.push_back(15);
     v.push_back(16);
 
-    // Pass vector to a function
-    print(v);
+    // mutating function
 
     return 0;
 }
