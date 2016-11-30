27d0b2f8805f861ab77b5451de8354ef38935441
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-30 08:22:01 -0500

    Print element #4 of v...

--- main.cpp
+++ main.cpp
@@ -4,18 +4,13 @@
 #include <algorithm>
 #include <iterator>
 
-extern "C"
-{
-    void legacy_function(int* pData, size_t size) {}
-}
-
 int main(int, const char*[])
 {
     std::vector<int> v = {23, 42, 4, 8};
     v.push_back(15);
     v.push_back(16);
 
-    legacy_function(v.data(), v.size());
+    // Print element #4 of v... 
 
     return 0;
 }
