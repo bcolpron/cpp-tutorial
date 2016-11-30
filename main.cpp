0f4dab2e923a6c94487c583f0e77912908f38335
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-30 08:51:17 -0500

    Let's talk about STL

--- main.cpp
+++ main.cpp
@@ -4,31 +4,13 @@
 #include <algorithm>
 #include <iterator>
 
-// The post-C++11 way (with move semantic)
-std::vector<int> twice(std::vector<int> v)
-{
-    for (int& i : v)
-    {
-        i *= 2;
-    }
-    return v;
-}
-
-std::vector<int> returns_a_vector();
-
 int main(int, const char*[])
 {
     std::vector<int> v = {23, 42, 4, 8};
     v.push_back(15);
     v.push_back(16);
 
-    // mutating function
-
-    // Ah...
-    v = twice(twice(twice(std::move(v))));
-
-    // handles temporary
-    twice(returns_a_vector());
+    // Let's talk about STL
 
     return 0;
 }
