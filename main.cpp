433d358c072bfa773e76e78401a899b87f0b387f
Bertin Colpron <bertin.colpron@kontron.com>
2016-11-30 08:15:30 -0500

    insert() example

--- main.cpp
+++ main.cpp
@@ -11,5 +11,7 @@ int main(int, const char*[])
     v.push_back(15);
     v.push_back(16);
 
+    v.insert(v.begin(), 1); // equivalent of "push_front()" (with some cost penalty!)
+
     return 0;
 }
