066bca4849018f99e654b68e4db4dd7570299c7f
Bertin Colpron <bcolpron78@gmail.com>
2016-12-03 07:24:20 -0500

    basic webservice class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include <map>
+
+class IBackend;
+
+class WebServiceImpl
+{
+	std::unique_ptr<IBackend> backend;
+public:
+
+	typedef unsigned short HttpStatusCode;
+	typedef std::map<std::string, std::string> Params;
+
+	/* a lot more of mission-specific code here */
+
+	// One of many URI dispatched request handler
+	HttpStatusCode reboot_device(const Params& params);
+};
+
 
 int main(int, const char*[])
 {
-    std::cout << "hello" << std::endl;
     return 0;
 }
