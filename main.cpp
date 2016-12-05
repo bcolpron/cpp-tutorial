4d8561a608657866235eacb4a0fabe5791f4eafb
Bertin Colpron <bcolpron78@gmail.com>
2016-12-05 09:03:14 -0500

    decorator using lambdas

--- main.cpp
+++ main.cpp
@@ -2,33 +2,29 @@
 #include <functional>
 #include <chrono>
 
-/////////////////////////////////////////////
-// API 
-/////////////////////////////////////////////
+// Decorator pattern!
+template<class F>
+auto decorate(F f)
+{
+    return [f{std::move(f)}] (auto... args)
+    {
+        std::cout << "before call" << std::endl;
+        f(args...);
+        std::cout << "after call" << std::endl;
+    };
+}
 
-typedef std::function<bool(float)> Progress;
 
-void long_running_task(Progress callback);
-
-/////////////////////////////////////////////
-/////////////////////////////////////////////
+void my_func()
+{
+    std::cout << "allo" << std::endl;
+};
 
 int main(int, const char*[])
 {
-    auto start = std::chrono::system_clock::now();
+    auto decorated = decorate(my_func);
 
-    auto callback = [=](float percent)
-    {
-        auto elapsed
-            = std::chrono::duration_cast<std::chrono::milliseconds>(
-                std::chrono::system_clock::now() - start);
-
-        std::cout << percent << "% elapsed in " << elapsed.count() << "ms" << std::endl;
-
-        return true;
-    };
-
-    long_running_task(callback);
+    decorated();
 
     return 0;
 }
