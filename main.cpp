63ad8717b8e4ea06bf66bdebcadafe53658bb7da
Bertin Colpron <bertin.colpron@kontron.com>
2016-12-06 13:00:36 -0500

    performance comment

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ struct NimporteQuoi
 {
     NimporteQuoi() {}
     NimporteQuoi(std::string n, double v): name(std::move(n)), value(v) {}
-    
+
     std::string name;
     double value;
 };
@@ -19,11 +19,11 @@ int main(int, const char*[])
     std::vector<NimporteQuoi> v(10);  // pre-filled vector of 10 default-constructed elements
 
     NimporteQuoi pi {"pi", 3.1416};
-    v.push_back(pi);
+    v.push_back(pi);                            // copy (decent)
 
-    v.push_back(NimporteQuoi{"e",  2.7183});
+    v.push_back(NimporteQuoi{"e",  2.7183});    // Move (better)
 
-    v.emplace_back("golden ration", 1.6180);
+    v.emplace_back("golden ration", 1.6180);    // in-place construction (awesome)
 
     return 0;
 }
