26874bbe38ba19e9f7a3a3ce2a2bc91248b4887a
Bertin Colpron <bertin.colpron@kontron.com>
2016-12-06 12:58:18 -0500

    copy, move insertion

--- main.cpp
+++ main.cpp
@@ -15,5 +15,10 @@ int main(int, const char*[])
 {
     std::vector<NimporteQuoi> v(10);  // pre-filled vector of 10 default-constructed elements
 
+    NimporteQuoi pi {"pi", 3.1416};
+    v.push_back(pi);
+
+    v.push_back(NimporteQuoi{"e",  2.7183});
+
     return 0;
 }
