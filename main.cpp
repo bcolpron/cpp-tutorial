322a203afd9f1b1b44866e14d2b267668e06a038
Bertin Colpron <bertin.colpron@kontron.com>
2016-12-07 08:43:31 -0500

    vector of polymorphic objects

--- main.cpp
+++ main.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <thread>
+#include <vector>
 
-struct IBlob
+struct Animal
 {
-    virtual ~IBlob() {};
+    virtual ~Animal() {};
+    virtual std::string sound() = 0; 
 };
 
-struct Blob: IBlob
+struct Dog: Animal
 {
-    ~Blob() override {}
-
-    std::string name = "Inigo Montoya";
-    /* More stuff here */
+    std::string sound() override {return "barl";}
 };
 
-struct BlobFactory
+struct Cat: Animal
 {
-    std::unique_ptr<IBlob> create_blob() { return std::make_unique<Blob>(); }
+    std::string sound() override {return "meow";}
 };
 
-BlobFactory factory;
+struct Cow: Animal
+{
+    std::string sound() override {return "moo";}
+};
+
 
 int main(int, const char *[])
 {
-    std::shared_ptr<IBlob> shared_blob(factory.create_blob());
+    // Gaaah! For goodness sake, never, ever do this!!
+    std::vector<Animal*> animals;
+    animals.push_back(new Dog);
+    animals.push_back(new Cat);
+    animals.push_back(new Cow);
+
+    for(auto i : animals) std::cout << i->sound() << std::endl;
+
+    for(auto i : animals) delete i;
 
     return 0;
 }
