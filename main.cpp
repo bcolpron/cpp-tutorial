5c3842d02397af9026cdb23daa609e7cb739c17c
Bertin Colpron <bertin.colpron@kontron.com>
2016-12-08 15:22:17 -0500

    gem for dec-9-2016 software team meeting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <thread>
+#include <memory>
 
-int main(int, const char*[])
+struct Alice
 {
-    std::cout << "hello" << std::endl;
-    return 0;
+    void hello() const { std::cout << "hello" << std::endl; }
+};
+
+struct Bob
+{
+    explicit Bob(std::shared_ptr<Alice> a): alice(std::move(a)){}
+
+    void greetings() { alice->hello(); }
+
+private:
+    std::shared_ptr<Alice> alice;
+};
+
+void foobar()
+{
+    auto alice = std::make_shared<Alice>();
+
+    Bob bob(alice);
+
+    std::thread([=]
+    {
+        alice->hello();
+    }).detach();
+
+    alice = nullptr;
+
+    bob.greetings();
 }
+
+// Questions:
+// - Ce programme est-il valide (i.e. exempt de bug) ?
+// - Que peut-on dire du cycle de vie de "Alice" ?
+
+
+
