e16b2d65b15a00628fcbf2ae0a663c3219e1b07f
Bertin Colpron <bertin.colpron@kontron.com>
2016-12-15 09:17:47 -0500

    GEM: map misuse and returning by reference

--- main.cpp
+++ main.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <cassert>
-#include <map>
-#include <thread>
-#include <iostream>
-#include <optional>
+// In AddressBook.h  /////////////////////////////////
 
 struct Contact
 {
@@ -11,98 +6,39 @@ struct Contact
     std::string phone;
 };
 
-typedef unsigned int ContactId; 
+using ContactId = unsigned int;
 
 class AddressBook
 {
 public:
-    ContactId add(const Contact& c);
-    Contact get(ContactId id) const;
-    void update(ContactId id, const Contact& c);
+    ContactId add(Contact c);
+    const Contact& get(ContactId id) const;
+    void update(ContactId id, Contact c);
     bool remove(ContactId id);
-
-    void addOrUpdate(ContactId id, const Contact& c);
-    std::optional<Contact> find(ContactId id) const;
-
 private:
     std::map<ContactId, Contact> m_contacts;
     ContactId m_sequence = 1;
     mutable std::mutex m_mutex;
 };
 
-ContactId AddressBook::add(const Contact& c)
+// In AddressBook.cpp ////////////////////////////////
+
+ContactId AddressBook::add(Contact c)
 {
     std::lock_guard<std::mutex> lock(m_mutex);
-    m_contacts[m_sequence] = c;
+    m_contacts[m_sequence] = std::move(c);
     return m_sequence++;
 }
 
-Contact AddressBook::get(ContactId id) const
+const Contact& AddressBook::get(ContactId id) const
 {
     std::lock_guard<std::mutex> lock(m_mutex);
-    auto found = m_contacts.find(id);
-    if (found != m_contacts.end())
-        return found->second;
-    else
-        throw std::out_of_range("not found");
+    return m_contacts[id];
 }
 
-void AddressBook::update(ContactId id, const Contact& c)
-{
-    std::lock_guard<std::mutex> lock(m_mutex);
-    auto found = m_contacts.find(id);
-    if (found != m_contacts.end())
-        found->second = c;
-    else
-        throw std::out_of_range("not found");
-}
+/* ... */
 
-bool AddressBook::remove(ContactId id)
-{
-    std::lock_guard<std::mutex> lock(m_mutex);
-    return m_contacts.erase(id) > 0;
-}
+// Ce code est-il valide (i.e. exempt de bug) ?
 
-void AddressBook::addOrUpdate(ContactId id, const Contact& c)
-{
-    std::lock_guard<std::mutex> lock(m_mutex);
-    auto found = m_contacts.insert(std::make_pair(id, c));
-    if (found.second)
-        ; // inserted!
-    else
-        found.first->second = c;
-}
 
-std::optional<Contact> AddressBook::find(ContactId id) const
-{
-    std::lock_guard<std::mutex> lock(m_mutex);
-    auto found = m_contacts.find(id);
-    if (found != m_contacts.end())
-        return found->second;
-    else
-        return {};
-}
 
-int main(int, const char*[])
-{
-    AddressBook book;
-
-    auto id = book.add(Contact{"Verbal Kint", "514-555-1234"});
-
-    auto guy = book.get(id);
-    assert(guy.name == "Verbal Kint");
-
-    guy.name = "Keyser Soze";
-    book.update(id, guy);
-
-    book.remove(id);
-
-    auto found = book.find(id);
-    if (found) {
-        std::cout << "Found " << found->name << std::endl;
-    } else {
-        std::cout << "not found" << std::endl;
-    }
-
-    return 0;
-}
