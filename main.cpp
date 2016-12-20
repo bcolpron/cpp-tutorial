34dd52993ac5e43b761026de13dc71f7b56c869c
Bertin Colpron <bertin.colpron@kontron.com>
2016-12-20 09:10:36 -0500

    visitor pattern implementation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+
+struct File;
+struct Folder;
+
+struct Visitor
+{
+    virtual void visit(File& e) = 0;
+    virtual void visit(Folder& e) = 0;
+};
+
+struct Element
+{
+    virtual ~Element() {}
+
+    std::string name() const;
+
+    virtual void accept(Visitor& v)=0;
+};
+
+struct File: Element
+{
+    std::size_t size() const; // File size in bytes
+
+    void accept(Visitor& v) override {v.visit(*this);}
+};
+
+struct Folder: Element
+{
+    std::vector<std::shared_ptr<Element>> children() const;
+    
+    void accept(Visitor& v) override {v.visit(*this);}
+};  
+
+struct SpaceCalculator: Visitor
+{
+    void visit(File& e) override
+    {
+        total_size += e.size();
+    }
+    void visit(Folder& e) override
+    {
+        for(auto&& child : e.children())
+        {
+            child->accept(*this);
+        }
+    };
+
+    std::size_t total_size;
+};
 
 int main(int, const char*[])
 {
-    std::cout << "hello" << std::endl;
+    std::vector<std::shared_ptr<Element>> root; // obtained somehow
+
+    SpaceCalculator visitor;
+    for(auto&& item: root) item->accept(visitor);
+
+    std::cout << "Total size: " << visitor.total_size << " bytes." << std::endl;
     return 0;
 }
