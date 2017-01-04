b667be4103f2b49d7b699168a8380294c0085c04
Bertin Colpron <bertin.colpron@kontron.com>
2017-01-04 09:17:01 -0500

    there it is

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <any>
 
-int main(int, const char*[])
+using namespace std::chrono_literals;
+
+namespace foo::bar
 {
-    std::cout << "hello" << std::endl;
-    return 0;
+	struct Event
+	{
+		std::string name;
+		std::any data;
+	};
+
+	class EventServiceConnection
+	{
+	public:
+		EventServiceConnection(std::string_view url); // connect to remote service
+
+		void submit_event(Event e); // push a new event to service
+
+		std::optional<Event> get_next_event(); // non-blocking request for event
+	};
+}
+
+void main_loop()
+{
+	foo::bar::EventServiceConnection service("somewhere://over-the-rainbow");
+
+	service.submit_event({"ready", std::pair(123, "demo-client") });
+
+	while (true)
+	{
+		if (auto e = service.get_next_event(); e != std::nullopt)
+		{
+			std::cout << e->name << std::any_cast<double>(e->data) << std::endl;
+		}
+		else
+		{
+			std::this_thread::sleep_for(1s);
+		}
+	}
 }
