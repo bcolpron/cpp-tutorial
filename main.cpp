9ff111f0cccd021cb72db3a6fdf9108c387d1d7d
Bertin Colpron <bertin.colpron@kontron.com>
2017-01-11 15:26:08 -0500

    first draft

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+extern std::string global_str;
+std::string function_returning_a_std_string();
+
+void foo(const std::string& s)  { printf(s.c_str()); }
+void foo(const char* s)         { printf(s);         }
+void foo(std::string s)         { printf(s.c_str()); }
+
+void bar(const std::string& s)  { global_str = s;            }
+void bar(const char* s)         { global_str = s;            }
+void bar(std::string s)         { global_str = std::move(s); }
+
+
 int main(int, const char*[])
 {
-    std::cout << "hello" << std::endl;
+    foo("this is a string literal");
+    bar(function_returning_a_std_string());
     return 0;
 }
