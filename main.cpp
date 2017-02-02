9acdb9faf4618a71df76a100064b12f78b12a7a4
Bertin Colpron <bertin.colpron@kontron.com>
2017-02-02 07:29:41 -0500

    add trivia question

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
+
+//
+//         ** Passing strings - the proper way **
+//
+//  What is best way (A, B or C) to pass string to the functions below?
+//
+
+extern "C" { void legacy(const char* s); }
+
+/* A */ void foo(const std::string& s)  { legacy(s.c_str()); }
+/* B */ void foo(const char* s)         { legacy(s        ); }
+/* C */ void foo(std::string s)         { legacy(s.c_str()); }
+
 extern std::string global_str;
-std::string function_returning_a_std_string();
 
-void foo(const std::string& s)  { printf(s.c_str()); }
-void foo(const char* s)         { printf(s);         }
-void foo(std::string s)         { printf(s.c_str()); }
-
-void bar(const std::string& s)  { global_str = s;            }
-void bar(const char* s)         { global_str = s;            }
-void bar(std::string s)         { global_str = std::move(s); }
+/* A */ void bar(const std::string& s)  { global_str = s;            }
+/* B */ void bar(const char* s)         { global_str = s;            }
+/* C */ void bar(std::string s)         { global_str = std::move(s); }          
 
 
 int main(int, const char*[])
@@ -18,3 +26,6 @@ int main(int, const char*[])
     bar(function_returning_a_std_string());
     return 0;
 }
+
+
+
