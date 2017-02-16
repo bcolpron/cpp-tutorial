e501ab0603ab7842a202b9f6ccd965dde48e0326
Bertin Colpron <bertin.colpron@kontron.com>
2017-02-16 11:03:37 -0500

    documentation

--- main.cpp
+++ main.cpp
@@ -5,36 +5,40 @@
 struct File;
 struct Folder;
 
+/// Visitor interface
 struct Visitor
 {
     virtual void visit(File& e) = 0;
     virtual void visit(Folder& e) = 0;
 };
 
+/// Base class for all 'FileSystem' elements 
 struct Element
 {
     virtual ~Element() {}
-
-    std::string name() const;
-
     virtual void accept(Visitor& v)=0;
+
+    std::string name() const;   // name of file/folder
 };
 
+/// Stripped-down file/document interface   
 struct File: Element
 {
-    std::size_t size() const; // File size in bytes
-
     void accept(Visitor& v) override {v.visit(*this);}
+
+    std::size_t size() const; // File size in bytes
 };
 
+/// Folder is a mere collection of 'Element's
 struct Folder: Element
 {
-    std::vector<std::shared_ptr<Element>> children() const;
-    
     void accept(Visitor& v) override {v.visit(*this);}
+
+    std::vector<std::shared_ptr<Element>> children() const;
 };  
 
-struct SpaceCalculator: Visitor
+/// One particular purpose operation...
+struct SizeCalculator: Visitor
 {
     void visit(File& e) override
     {
@@ -48,16 +52,23 @@ struct SpaceCalculator: Visitor
         }
     };
 
-    std::size_t total_size;
+    std::size_t total_size = 0;
 };
 
 int main(int, const char*[])
 {
     std::vector<std::shared_ptr<Element>> root; // obtained somehow
 
-    SpaceCalculator visitor;
-    for(auto&& item: root) item->accept(visitor);
+    SizeCalculator visitor;
+    for(auto&& item: root)
+    {
+        item->accept(visitor);
+    }
 
     std::cout << "Total size: " << visitor.total_size << " bytes." << std::endl;
     return 0;
 }
+
+
+// The Visitor Pattern - Is this good OO design/implementation?
+
